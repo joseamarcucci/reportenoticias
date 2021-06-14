dfa486b6e16b38575a0bf40c12003d5099822f94
joseamarcucci <74363457+joseamarcucci@users.noreply.github.com>
2021-06-14 12:10:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 from sqlalchemy import create_engine
 import streamlit as st
 import pandas as pd
-
 import re 
 import socket 
+st.markdown("<h2 style='text-align: left; color: #00b8e1;'>Reporte de envio boletín</h2>", unsafe_allow_html=True)
+buff1,buff, col = st.beta_columns([1,2,2])
+display_code =   buff1.radio("Mostrar", ( "Envios","Rebotes", "Suscriptores"))
 hostname = socket.gethostname()
 local_ip = socket.gethostbyname(hostname)
 db_connection_str = 'mysql+pymysql://clayssorg_orgar_2020:anitA&2020@69.16.228.38:3306/clayssorg_noti_ext'
 db_connection = create_engine(db_connection_str)
 
 df = pd.read_sql('SELECT * FROM watchdog where type="mail"', con=db_connection)
-#df2 = pd.read_sql('SELECT * FROM simplenews_subscriber', con=db_connection)
-#s=df["variables"]
+df2 = pd.read_sql('SELECT * FROM simplenews_subscriber', con=db_connection)
+
 #lst = re.findall('\S+@\S+', s) 
 st.write(local_ip)
-st.table(df)
+if display_code == "Envios":
+   st.table(df)
+if display_code == "Suscriptores":
+   st.table(df2)
