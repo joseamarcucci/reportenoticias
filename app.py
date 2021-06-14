3551e3fd774e57e7c6e4da984962e60f2731fedb
joseamarcucci <74363457+joseamarcucci@users.noreply.github.com>
2021-06-14 11:58:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,16 @@ from sqlalchemy import create_engine
 import streamlit as st
 import pandas as pd
 
-db_connection_str = 'mysql+pymysql://usal_orientate:pablO&2020@69.16.228.38:3306/usal_noticiasok'
+import re 
+import socket 
+hostname = socket.gethostname()
+local_ip = socket.gethostbyname(hostname)
+db_connection_str = 'mysql+pymysql://clayssorg_orgar_2020:anitA&2020@69.16.228.38:3306/clayssorg_noti_ext'
 db_connection = create_engine(db_connection_str)
 
-df = pd.read_sql('SELECT * FROM config', con=db_connection)
+df = pd.read_sql('SELECT * FROM watchdog where type="mail"', con=db_connection)
+#df2 = pd.read_sql('SELECT * FROM simplenews_subscriber', con=db_connection)
+#s=df["variables"]
+#lst = re.findall('\S+@\S+', s) 
+st.write(local_ip)
 st.table(df)
