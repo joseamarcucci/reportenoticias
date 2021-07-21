25150c863fa2e25aca1e9f1221879544780ccfdd
joseamarcucci <74363457+joseamarcucci@users.noreply.github.com>
2021-07-21 11:55:33 -0300

    Update flyer.py

--- flyer.py
+++ flyer.py
@@ -149,7 +149,7 @@ if display_code=='Enviar Boletín':
     r = requests.get(url)
 
 
-    components.html(soup1, width=1000, height=5500)
+    components.html(soup1, width=1000, height=7500)
 
     if st.sidebar.button('Enviar'):
       for row_value in row_values_list:
